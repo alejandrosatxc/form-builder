3aa326bfb11a2ced03e7abddee4186796b197a8e
alejandrosatxc <alejandro@satxconsultants.com>
2022-10-20 15:39:42 -0500

    Wrap <nav> in <header>

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,13 +1,15 @@
 const Navbar = () => {
-    return(
-        <nav className="bg-white h-20 w-full flex justify-between items-center">
-            <ul className="flex w-auto">
-                <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/">Form Builder</a></li>
-                <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/upload">Upload</a></li>
-                <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/preview">Preview</a></li>
-            </ul>
-            <h1 className="text-4xl p-4"><a href="/">✨🦆</a></h1>
-        </nav>
+    return (
+        <header>
+            <nav className="bg-white h-20 w-full flex justify-between items-center">
+                <ul className="flex w-auto">
+                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/">Form Builder</a></li>
+                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/upload">Upload</a></li>
+                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/preview">Preview</a></li>
+                </ul>
+                <h1 className="text-4xl p-4"><a href="/">✨🦆</a></h1>
+            </nav>
+        </header>
     )
 }
 
