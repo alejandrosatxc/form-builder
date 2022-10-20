60ee5c606d7d2dd0a62a839ad00a752d3abfb592
alejandrosatxc <alejandro@satxconsultants.com>
2022-10-20 15:49:15 -0500

    Move <a> to wrap <li>'s to improve click targets

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -3,9 +3,9 @@ const Navbar = () => {
         <header>
             <nav className="bg-white h-20 w-full flex justify-between items-center">
                 <ul className="flex w-auto">
-                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/">Form Builder</a></li>
-                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/upload">Upload</a></li>
-                    <li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400"><a href="/preview">Preview</a></li>
+                    <a href="/"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Form Builder</li></a>
+                    <a href="/upload"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Upload</li></a>
+                    <a href="/preview"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Preview</li></a>
                 </ul>
                 <h1 className="text-4xl p-4"><a href="/">✨🦆</a></h1>
             </nav>
