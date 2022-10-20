061ec04aa11bd91ccac7f339e1c18798a424f936
alejandrosatxc <alejandro@satxconsultants.com>
2022-10-20 16:32:47 -0500

    Create Droppable Trash component for FormBuilder

--- components/FormBuilder.js
+++ components/FormBuilder.js
@@ -1,11 +1,13 @@
 import Dustbin from '../components/Dustbin'
+import Trashcan from './Trashcan'
 
 const FormBuilder = () => {
         
     return (
-        <div className="col-span-2 bg-white rounded-xl shadow-xl h-full w-full p-6">
-            <h1 className="text-6xl text-center border-b-2">Form Builder</h1>
-            <Dustbin />
+        <div className="col-span-2 flex flex-col bg-white rounded-xl shadow-xl h-full w-full p-6">
+            <h1 className="text-6xl text-center border-b-2 my-2">Form Builder</h1>
+            <Dustbin/>
+            <Trashcan />
         </div>
     )
 }
---
+++ components/Trashcan.js
@@ -0,0 +1,43 @@
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from './FormComponents/Constants'
+const style = {
+    // height: '90%',
+    width: '100%',
+    marginRight: '1.5rem',
+    marginBottom: '1.5rem',
+    color: 'white',
+    padding: '1rem',
+    textAlign: 'center',
+    fontSize: '1rem',
+    lineHeight: 'normal',
+    float: 'left',
+  }
+const Trashcan = () => {
+
+    const [{ canDrop, isOver }, drop] = useDrop(() => ({
+
+        accept: ItemTypes.INPUT,
+        drop: () => ({ name: 'Dustbin' }),
+        collect: (monitor) => ({
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+        }),
+    }))
+
+    const isActive = canDrop && isOver
+    let backgroundColor = '#222'
+    if (isActive) {
+      backgroundColor = 'darkgreen'
+    } else if (canDrop) {
+      backgroundColor = 'darkkhaki'
+    }
+
+    return (
+        <div className="mt-auto" ref={drop} style={{ ...style, backgroundColor }} data-testid="trashcan">
+            {isActive ? 'Release to drop' : 'Drag a box here'}
+            <h3 className="text-center">Trash</h3>
+        </div>
+    )
+}
+
+export default Trashcan
