fb9b6d4d3741429d02d83f643cb6cce33acc12d7
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-17 17:07:31 -0600

    Change <a> to <Link> add pointer css

--- components/layout/navbar.js
+++ components/layout/navbar.js
@@ -1,11 +1,13 @@
+import Link from "next/link"
+
 const Navbar = () => {
     return (
         <header className="h-20">
             <nav className="bg-white h-full w-full flex justify-between items-center">
                 <ul className="flex w-auto">
-                    <a href="/"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Form Builder</li></a>
-                    <a href="/upload"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Upload</li></a>
-                    <a href="/preview"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 transition ease-in-out delay-300 hover:bg-blue-400">Preview</li></a>
+                    <Link href="/"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 cursor-pointer transition ease-in-out hover:bg-blue-400">Form Builder</li></Link>
+                    <Link href="/upload"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 cursor-pointer transition ease-in-out hover:bg-blue-400">Upload</li></Link>
+                    <Link href="/preview"><li className="bg-gray-200 rounded-lg h-10 text-center mx-4 p-2 cursor-pointer transition ease-in-out hover:bg-blue-400">Preview</li></Link>
                 </ul>
                 <h1 className="text-4xl p-4"><a href="/">✨🦆</a></h1>
             </nav>
