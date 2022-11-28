38eac4a86ffe9b4a8b779a701c0ff186f7e785f2
Alejandro Zapien <alejandro@satxconsultants.com>
2022-11-28 14:06:50 -0600

    Complete google Oauth/fetch googleDoc workflow

--- components/GDocUploader.js
+++ components/GDocUploader.js
@@ -1,16 +1,55 @@
+import { useSession, signIn, signOut } from "next-auth/react"
+import { useRef } from "react"
+
 const GDocUploader = () => {
+    const { data: session } = useSession()
+    const inputRef = useRef(null)
+
+    const handleGdocSubmit = () => {
+        console.log(session)
+        const regex = /\/document\/d\/([a-zA-Z0-9-_]+)/
+        var baseURL = 'https://docs.googleapis.com/v1/documents/'
+        var userInput = inputRef.current.value
+        var documentId = regex.exec(userInput)
+        console.log(documentId[1])
+        console.log(baseURL + documentId[1])
+        fetch(baseURL + documentId[1],{
+            method: 'GET',
+            headers: new Headers ({
+                'Authorization' : 'Bearer ' + session.accessToken
+            })
+        })
+        .then(res => {
+            console.log(res)
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
 
     return (
         <div className="col-span-3 flex flex-col justify-center items-center bg-white rounded-xl shadow-xl h-1/2 w-3/4">
-            <label className="h-8 mx-auto" htmlFor="url">Enter an Google Doc URL:</label>
-            <input className="border-2 my-4 mx-auto" type="url" name="url" id="url"
+            <label className="h-8 mx-auto" htmlFor="url">Enter a Google Doc URL:</label>
+            <input className="border-2 my-4 mx-auto" ref={inputRef} type="url" name="url" id="url"
                 placeholder="https://example.com"
                 pattern="https://.*" size="30"
                 required>    
             </input>
-            <button className="transition ease-in-out delay-150 bg-blue-300 hover:bg-blue-600 hover:shadow-xl hover:shadow-cyan-200 h-8 w-16 mx-auto rounded-lg" type="submit">
+            <button onClick={handleGdocSubmit} className="transition ease-in-out delay-150 bg-blue-300 hover:bg-blue-600 hover:shadow-xl hover:shadow-cyan-200 h-8 w-16 mx-auto rounded-lg" type="submit">
                 Submit
             </button>
+            {session 
+                ? 
+                <div>  Signed in as {session.user.email} <br />
+                <button className="bg-slate-200 rounded p-2" onClick={() => signOut()}>Sign out</button></div> 
+                : 
+                <div>  Not signed in <br />
+                <button className="bg-slate-200 rounded p-2" onClick={() => signIn()}>Sign in</button></div>
+            }
         </div>
     )
 }
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,29 +1,41 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 export const authOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
+    // Configure one or more authentication providers
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+            idToken: true,
+            authorization: {
+                prompt: "consent",
+                access_type: "offline",
+                response_type: "code",
+                url: 'https://accounts.google.com/o/oauth2/v2/auth',
+                params: { scope: 'openid email profile https://www.googleapis.com/auth/documents https://www.googleapis.com/auth/documents.readonly'}
+            }
+        }),
 
-    // ...add more providers here
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    },
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.accessToken = token.accessToken
-      return session
+        // ...add more providers here
+    ],
+    secret: process.env.JWT_SECRET,
+    callbacks: {
+        async jwt({ token, account, profile }) {
+            // Persist the OAuth access_token to the token right after signin
+            if (account) {
+                token.accessToken = account.access_token
+                token.idToken = profile.idToken
+            }
+            return token
+        },
+        async session({ session, token, profile, user }) {
+            // Send properties to the client, like an access_token from a provider.
+            session.accessToken = token.accessToken
+            session.idToken = token.idToken
+            return session
+        }
     }
-  }
-  
+
 }
 export default NextAuth(authOptions)
+
