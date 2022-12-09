a2ac72d76cd8da6df8908384614f6eb031a452cf
Alejandro Zapien <alejandro@satxconsultants.com>
2022-12-09 05:51:23 -0600

    Change color on shadow-bump

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,8 +17,8 @@ module.exports = {
         'height' : 'height'
       },
       boxShadow: {
-        'well': 'inset 0 2px 2px hsla(0, 0%, 0%, 0.1), inset 0 -3px 0 hsla(0, 0%, 100%, .15)',
-        'bump': 'inset 0 2px 0 hsl(270, 84%, 80%), 0 1px 3px hsla(0, 0%, 0%, .2)',
+        'well': 'inset 0 2px 2px hsla(0, 0%, 0%, 0.1), inset 0 -2px 0 hsla(0, 0%, 100%, .15)',
+        'bump': 'inset 0 2px 0 hsl(250, 84%, 80%), 0 1px 3px hsla(0, 0%, 0%, .2)',
       }
     },
   },
