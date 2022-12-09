3d8ab09c102886b638d00436ccaff15a30ae3436
Alejandro Zapien <alejandro@satxconsultants.com>
2022-12-09 14:51:22 -0600

    Add some font-awesome icons!

--- components/TemplateAnalysis.js
+++ components/TemplateAnalysis.js
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { extractFields } from '../lib/getGdocText'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileLines, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
 
 const TemplateAnalysis = ({ Gdoc, setGdoc }) => {
 
@@ -43,19 +46,19 @@ const TemplateAnalysis = ({ Gdoc, setGdoc }) => {
     return (
         <div className="flex flex-col justify-center bg-white rounded-xl shadow-xl my-8 min-w-[550px]">
             <h2 className="text-lg text-slate-400 mt-8 mb-2 mx-8">Template Analysis</h2>
-            <h1 className="text-4xl text-primary mt-2 mb-4 mx-8">{GdocData.title}</h1>
+            <h1 className="text-4xl text-primary mt-2 mb-4 mx-8"><FontAwesomeIcon icon={faFileLines} className="mr-4" size="sm"/>{GdocData.title}</h1>
             <div className="flex flex-row justify-center w-full">
-                <div className='border rounded shadow-well bg-slate-100 border-slate-300 ml-8 mr-4 p-2 h-20 w-1/2'>
+                <div className='border rounded shadow-well bg-violet-200 border-slate-300 ml-8 mr-4 p-2 h-20 w-1/2'>
                     <h2 className='text-primary text-3xl'>{GdocData.uniqueMatches.length}</h2>
                     <p className='text-slate-600'>Unique Fields Identified</p>
                 </div>
-                <div className='border rounded shadow-well bg-slate-100 border-slate-300 ml-4 mr-8 p-2 h-20 w-1/2'>
+                <div className='border rounded shadow-well bg-violet-200 border-slate-300 ml-4 mr-8 p-2 h-20 w-1/2'>
                     <h2 className='text-primary text-3xl'>{GdocData.matches.length}</h2>
                     <p className='text-slate-600'>Total Fields</p>
                 </div>
             </div>
             {/* TODO: Make h2 stay purple when toggled */}
-            <h2 onClick={() => { setFieldsToggle(!fieldsToggle) }} className={fieldsToggle ? activeTextToggle : inactiveTextToggle}>{fieldsToggle ? "Fields -" : "Fields +"}</h2>
+            <h2 onClick={() => { setFieldsToggle(!fieldsToggle) }} className={fieldsToggle ? activeTextToggle : inactiveTextToggle}>Fields <FontAwesomeIcon className={fieldsToggle ? "transition ease-in-out duration-400 -rotate-180" : "transition ease-in-out duration-400"} icon={faChevronDown} size="sm"/></h2>
             <div className={fieldsToggle ? active : inactive}>
                 <ol className='list-decimal ml-8'>
                     {GdocData.uniqueMatches.map((field, index) => { 
