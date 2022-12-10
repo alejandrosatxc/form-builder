b03909dbdcb58f1b57d7e43911799f5bee67a8b6
Alejandro Zapien <alejandro@satxconsultants.com>
2022-12-10 15:41:09 -0600

    Layout adjustments

--- components/FormComponents/Name.js
+++ components/FormComponents/Name.js
@@ -3,9 +3,9 @@ const Name = ({title, id}) => {
     return (
         <>
             <h3 className="text-black text-xl">Name</h3>
-            <label className="text-black" htmlFor={id + '-firstName'} >First:</label>
+            <label className="text-black" htmlFor={id + '-firstName'} >First</label>
             <input className="text-black bg-slate-100 rounded shadow-well" id={id + '-firstName'} type="text"></input>
-            <label className="text-black"htmlFor={id + '-lastName'}>Last:</label>
+            <label className="text-black" htmlFor={id + '-lastName'}>Last</label>
             <input className="text-black bg-slate-100 rounded shadow-well" id={id + '-lastName'} type="text"></input>
         </>
     )
--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,20 @@ const Upload = () => {
     const [Gdoc, setGdoc] = useState(null)
 
     return (
-        <div className="flex flex-col justify-center place-items-center">
-        {Gdoc 
-        ?   <TemplateAnalysis setGdoc={setGdoc} Gdoc={Gdoc}/>
-        :   <GDocUploader setGdoc={setGdoc}/>
-        }
+        <>
+            <div className='flex flex-col justify-center place-items-center w-full h-64 bg-slate-300'>
+                {Gdoc
+                    ? <TemplateAnalysis setGdoc={setGdoc} Gdoc={Gdoc} />
+                    : <GDocUploader setGdoc={setGdoc} />
+                }
+            </div>
             <div className='w-full h-96 shadow-inner bg-violet-400'>
-                
+
             </div>
             <div className='w-full h-96 bg-violet-800'>
-                
+
             </div>
-        </div>
+        </>
     )
 }
 
