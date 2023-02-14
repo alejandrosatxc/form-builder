e386d03182566930d48f7245dbbdea66e4902036
Alejandro Zapien <alejandro@satxconsultants.com>
2023-02-14 01:04:25 -0600

    Update types, create form post route

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,16 +4,20 @@ declare module "next-auth" {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
-  interface Session { //interfaces can always be extended
-    accessToken?: string,
+   interface Session { //interfaces can always be extended
+    accessToken?: string
+    idToken: string
   }
-  interface JWT {
-    /** OpenID ID Token */
-    accessToken?: string,
-    idToken?: string
-  }
-
   interface Profile {
+    idToken: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    idToken?: string 
+    accessToken?: string
     id: string
+    role: number
   }
 }
